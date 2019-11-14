6c77d3f65155b686f98cbffd1df9bfb1a4b26afc
Tushar Khunt <tusharkhunt121@gmail.com>
2019-11-14 10:13:59 -0500

    Updates

---
+++ precache-manifest.7ca06566c00aa8c0ed52a13464fa5ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea18c11f40caeb948a8177e7c1424f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c2dce2b99de2a73453",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "5f46883d43a673e15f80",
+    "url": "/static/js/2.c5ec40bc.chunk.js"
+  },
+  {
+    "revision": "c2c2dce2b99de2a73453",
+    "url": "/static/js/main.1a80db49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
